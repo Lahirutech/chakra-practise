35d2062e52f59385628087987033118379268c2e
Lanka Rathanyaka <rmllbrathnayaka@gmail.com>
2022-10-15 19:34:37 +0530

    add Info section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import About from './components/About';
+import Info from './components/Info';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <About />
+      <Info />
     </ChakraProvider>
   );
 }
