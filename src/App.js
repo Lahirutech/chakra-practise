21178858f813b8f0a0ee55acd4b4abce5c4d7c28
Lanka Rathanyaka <rmllbrathnayaka@gmail.com>
2022-10-19 23:13:38 +0530

    add join our team

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import About from './components/About';
 import Info from './components/Info';
+import AuthOne from './components/AuthOne';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <About />
       <Info />
+      <AuthOne />
     </ChakraProvider>
   );
 }
